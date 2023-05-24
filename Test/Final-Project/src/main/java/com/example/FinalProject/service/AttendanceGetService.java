f7cae092393f55ac35922e5ca67d90734bc2117b
Md. Khairul Islam <khairulislamasif18@gmail.com>
2023-05-24 12:25:12 -0700

    home change

--- Test/Final-Project/src/main/java/com/example/FinalProject/service/AttendanceGetService.java
+++ Test/Final-Project/src/main/java/com/example/FinalProject/service/AttendanceGetService.java
@@ -2,9 +2,14 @@ package com.example.FinalProject.service;
 
 import com.example.FinalProject.empdto.AttendanceGetDto;
 import com.example.FinalProject.repository.AttendanceGetRepo;
+import javafx.util.converter.LocalDateStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -12,6 +17,8 @@ public class AttendanceGetService {
 
     @Autowired
     AttendanceGetRepo attendanceGetRepo;
+
+    LocalDate currentDate = LocalDate.now(Clock.systemDefaultZone()) ;
 
 //    public List<AttendanceGetDto> attenGet() {
 //        return attendanceGetRepo.findAllAttendance();
@@ -22,6 +29,25 @@ public class AttendanceGetService {
     }
 
     public List<AttendanceGetDto> absentGet() {
-        return attendanceGetRepo.findAbsent();
+
+        List<AttendanceGetDto> absentEmp = attendanceGetRepo.findAbsent();
+        List<AttendanceGetDto> withDateAbsent = new ArrayList<>();
+            for (AttendanceGetDto emp: absentEmp) {
+                if (emp.getDate() == null) {
+                    emp.setDate(this.currentDate);
+                    emp.setStatus("Absent");
+                    withDateAbsent.add(emp);
+
+                }
+
+
+            }
+        return withDateAbsent;
+
     }
+
+//    public void setCurrentDate() {
+//        this.currentDate = LocalDate.now(Clock.systemDefaultZone()) ;
+//
+//    }
 }
